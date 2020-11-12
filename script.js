8c773be36c0a7e5543320585ebb99dae48933fcf
Marie Wagle <mariewagle@maries-mbp.lan>
2020-11-12 16:33:00 +0100

    add burger menu

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// BURGER MENU 
 const menu = document.querySelector(".navbar-links");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
@@ -11,34 +12,17 @@ cancelBtn.onclick = ()=> {
 }
 
 
+// HEADER SCROLL
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header-nav").style.top = "0";
+  } else {
+    document.getElementById("header-nav").style.top = "-50rem";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const mainMenu = document.querySelector('.navbar-links');
-//const closeMenu = document.querySelector('.close-menu');
-//const openMenu = document.querySelector('.burger-menu');
-
-//openMenu.addEventListener('click',show);
-//closeMenu.addEventListener('click',close);
-
-//function show(){
- // mainMenu.style.display = 'flex';
-//mainMenu.style.top = '0';
-//}
-
-//function close(){
- // mainMenu.style.top = '-100%';
-//}
 
